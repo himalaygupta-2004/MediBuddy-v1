02f548d4939ed7853b3aedf6e2067fc83ca529fa
Mohammad Hashim <hashimmohammad1375@gmail.com>
2025-05-07 10:31:44 +0530

    Added requirement.txt

--- memory_llm.py
+++ memory_llm.py
@@ -4,6 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
+
 load_dotenv()
 
 def load_pdf_files(directory_path):
